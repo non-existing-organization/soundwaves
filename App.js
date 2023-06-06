e12b7383e9c1f6e4e65e30426f4721bdfe776c00
John Stilia <stilia.johny@gmail.com>
2023-06-06 21:41:23 +0100

    feat: Sound play depends on button
    Former-commit-id: 8b6983fbc1b345d94c4948ec81ec149367e9541b

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-// App.js
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, ImageBackground } from 'react-native';
+import { Audio } from 'expo-av';
 import CustomButton from './CustomButton';
 import colorMap from './colorMap';
 import styles from './styles';
@@ -10,18 +9,62 @@ const backgroundImage = require('./assets/background.png');
 
 const App = () => {
   const [mainImage, setMainImage] = useState(backgroundImage);
+  const [sound, setSound] = useState(null);
+  const [currentAudioFile, setCurrentAudioFile] = useState(null);
 
-  const handleButtonPress = (image) => {
+  const handleButtonPress = async (image, audio_file) => {
     setMainImage(image);
+
+    if (sound) {
+      if (audio_file === currentAudioFile) {
+        const isPlaying = await sound.getStatusAsync();
+        if (isPlaying.isPlaying) {
+          console.log('Pausing sound');
+          await sound.pauseAsync();
+        } else {
+          console.log('Resuming sound');
+          await sound.playAsync();
+        }
+      } else {
+        console.log('Stopping and unloading previous sound');
+        await sound.stopAsync();
+        await sound.unloadAsync();
+
+        console.log('Loading new sound');
+        const { sound: newSound } = await Audio.Sound.createAsync(audio_file);
+        setSound(newSound);
+        setCurrentAudioFile(audio_file);
+        console.log('Playing sound');
+        await newSound.playAsync();
+      }
+    } else {
+      console.log('Loading new sound');
+      const { sound: newSound } = await Audio.Sound.createAsync(audio_file);
+      setSound(newSound);
+      setCurrentAudioFile(audio_file);
+      console.log('Playing sound');
+      await newSound.playAsync();
+    }
   };
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound on component unmount');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const sortedColorMap = Array.from(colorMap).sort(([colorA], [colorB]) => colorA.localeCompare(colorB));
 
   return (
     <ImageBackground source={mainImage} style={styles.backgroundImage}>
       <View style={styles.buttonContainer}>
-        {Array.from(colorMap).map(([colorName, { name, image, thumbnail }]) => (
+        {sortedColorMap.map(([colorName, { name, image, thumbnail, audio_file }]) => (
           <View key={colorName}>
             <CustomButton
-              onPress={handleButtonPress}
+              onPress={() => handleButtonPress(image, audio_file)}
               name={name}
               image={image}
               thumbnail={thumbnail}
