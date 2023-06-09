e31b4142f2ed53d9408b744320b3407d8c6a191a
John Stilia <stilia.johny@gmail.com>
2023-06-09 08:16:27 +0100

    feat: add eslint config for react and browser* chore: add dev dependencies for eslint* feat: Add color highlight on button press* docs: Update AboutScreen description text* style: Change font family in aboutText to Roboto-Regular* feat: Add expo-app-loading and expo-font to dependencies* feat: Add Expo KeepAwake to App.js* style: Change font family to HelveticaNeue-Light* chore: Add expo-keep-awake to dependencies* style: reformat styles.js* feat: add ESLint linting for pull requests* style: Hide header in Stack.Navigator

--- AboutScreen.js
+++ AboutScreen.js
@@ -7,9 +7,9 @@ const AboutScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.aboutText}>
-        {"Each color of noise possesses distinct frequencies and sounds that can influence our brains in different ways. White and Brown noises, with their consistent, gentle hum, can help mask disturbing ambient sounds, promoting better focus and more restful sleep.\n\n"}
-        {"Pink noise, often likened to rainfall or wind, exhibits equal energy across octaves and can enhance brain stability and improve sleep quality.\n\n"}
-        {"Blue and Grey noises, with their higher frequencies, can potentially assist in enhancing concentration and alertness.\n\n"}
+        {"Each color of noise possesses distinct frequencies and sounds that can influence our brains in different ways. White and Brown noises, with their consistent, gentle hum, can help mask disturbing ambient sounds, promoting better focus and more restful sleep.\n\n\n\n"}
+        {"White noise, often compared to the gentle hiss of a radio tuned to an unused frequency, distributes energy equally across all audible frequencies, providing a soothing backdrop that can enhance focus and facilitate restful sleep.\n\n"}
+        {"Brown noise, commonly associated with the calming rumble of a waterfall or distant thunder, presents greater energy at lower frequencies, offering a deep, ambient sound that may foster relaxation and aid in promoting deeper sleep.\n\n\n\n"}
         {"Through regular exposure to these therapeutic sounds, individuals may experience reduced stress, improved productivity, and better overall well-being."}
       </Text>
       <TouchableOpacity style={styles.aboutButton} onPress={() => navigation.navigate('Main')}>
