6ee5b44944c8eff8695b17e43423e18fd6ff5b12
John Stilia <stilia.johny@gmail.com>
2023-06-10 14:23:54 +0100

    refactor: Simplify CustomButton PropTypes

--- CustomButton.js
+++ CustomButton.js
@@ -1,9 +1,10 @@
 // CustomButton.js
 import React from 'react';
-import { TouchableOpacity, Image, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import {TouchableOpacity, Image, Text} from 'react-native';
 import styles from './styles';
 
-const CustomButton = ({ onPress, name, image, thumbnail, isActive }) => {
+const CustomButton = ({onPress, name, thumbnail, isActive}) => {
   return (
     <TouchableOpacity
       onPress={onPress}
@@ -16,6 +17,13 @@ const CustomButton = ({ onPress, name, image, thumbnail, isActive }) => {
       <Text style={styles.buttonText}>{name}</Text>
     </TouchableOpacity>
   );
+};
+
+CustomButton.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  thumbnail: PropTypes.number.isRequired,
+  isActive: PropTypes.bool.isRequired,
 };
 
 export default CustomButton;
