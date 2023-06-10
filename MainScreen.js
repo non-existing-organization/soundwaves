4adc87a0c6ec02c06c05040d1dd48ac955744f40
John Stilia <stilia.johny@gmail.com>
2023-06-10 14:25:35 +0100

    refactor: simplify useEffect and variable names

--- MainScreen.js
+++ MainScreen.js
@@ -16,95 +16,95 @@ const MainScreen = () => {
   const [activeColor, setActiveColor] = useState(null); // Add this state
 
   useEffect(() => {
-    return sound
-      ? () => {
-          console.log("Unloading Sound on component unmount");
-          sound.unloadAsync();
-        }
+    return sound ?
+      () => {
+        console.log('Unloading Sound on component unmount');
+        sound.unloadAsync();
+      }
       : undefined;
   }, [sound]);
 
   useEffect(() => {
-    Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
+    Audio.setAudioModeAsync({playsInSilentModeIOS: true});
   });
 
-  const handleButtonPress = async (colorName, image, audio_file) => {
+  const handleButtonPress = async (colorName, image, audioFile) => {
     console.log(
-        `Button press detected. Color: ${colorName}, Image: ${image}, Audio File: ${audio_file}`
+        `Button press detected. Color: ${colorName}, Image: ${image}, Audio File: ${audioFile}`
     );
     setMainImage(image);
 
-    const isOtherSoundPlaying = sound && audio_file !== currentAudioFile;
+    const isOtherSoundPlaying = sound && audioFile !== currentAudioFile;
     const isSoundLoaded = sound && (await sound.getStatusAsync()).isLoaded;
 
     if (sound && isSoundLoaded) {
       const status = await sound.getStatusAsync();
       if (isOtherSoundPlaying) {
-        console.log("Stopping and unloading previous sound");
+        console.log('Stopping and unloading previous sound');
         await sound.stopAsync();
         await sound.unloadAsync();
-        console.log("Previous sound unloaded");
+        console.log('Previous sound unloaded');
         setActiveColor(null); // Add this line
       } else if (status.isPlaying) {
-        console.log("Pausing current sound");
+        console.log('Pausing current sound');
         await sound.pauseAsync();
-        console.log("Current sound paused");
+        console.log('Current sound paused');
         setActiveColor(null); // Add this line
         return;
       } else {
-        console.log("Resuming current sound");
+        console.log('Resuming current sound');
         await sound.playAsync();
-        console.log("Current sound resumed");
+        console.log('Current sound resumed');
         setActiveColor(colorName); // Add this line
         return;
       }
     }
 
-    console.log("Loading new sound");
-    const { sound: newSound } = await Audio.Sound.createAsync(
-      audio_file,
-      {
-        isLooping: true,
-        isMuted: false,
-        volume: 1.0,
-        rate: 1.0,
-        shouldCorrectPitch: true,
-      },
-      (status) => {
-        if (status.didJustFinish && !status.isLooping) {
-          setLoopCount(loopCount + 1);
-          console.log("Loop count:", loopCount);
-        }
-      }
+    console.log('Loading new sound');
+    const {sound: newSound} = await Audio.Sound.createAsync(
+        audioFile,
+        {
+          isLooping: true,
+          isMuted: false,
+          volume: 1.0,
+          rate: 1.0,
+          shouldCorrectPitch: true,
+        },
+        (status) => {
+          if (status.didJustFinish && !status.isLooping) {
+            setLoopCount(loopCount + 1);
+            console.log('Loop count:', loopCount);
+          }
+        },
     );
     setSound(newSound);
-    setCurrentAudioFile(audio_file);
+    setCurrentAudioFile(audioFile);
     setActiveColor(colorName); // Add this line
-    console.log("New sound loaded");
+    console.log('New sound loaded');
 
-    console.log("Playing new sound");
+    console.log('Playing new sound');
     await newSound.playAsync();
-    console.log("New sound playing");
+    console.log('New sound playing');
   };
 
   const sortedColorMap = Array.from(colorMap).sort(([colorA], [colorB]) =>
-    colorA.localeCompare(colorB)
+    colorA.localeCompare(colorB),
   );
 
   return (
     <ImageBackground source={mainImage} style={styles.backgroundImage}>
       <View style={styles.buttonContainer}>
         {sortedColorMap.map(
-          ([colorName, { name, image, thumbnail, audio_file }]) => (
-            <View key={colorName}>
-              <CustomButton
-                onPress={() => handleButtonPress(colorName, image, audio_file)}
-                image={image}
-                thumbnail={thumbnail}
-                isActive={activeColor === colorName}
-              />
-            </View>
-          )
+            ([colorName, { name, image, thumbnail, audioFile }]) => (
+              <View key={colorName}>
+                <CustomButton
+                  onPress={() => handleButtonPress(colorName, image, audioFile)}
+                  image={image}
+                  thumbnail={thumbnail}
+                  isActive={activeColor === colorName}
+                />
+              </View>
+            ),
         )}
       </View>
     </ImageBackground>
