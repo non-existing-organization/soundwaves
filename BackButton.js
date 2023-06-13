c7a0226b975163394963653130cf82959c1ada1d
John Stilia <stilia.johny@gmail.com>
2023-06-14 00:02:52 +0100

    feat: Add BackButton and SettingsButton components

---
+++ BackButton.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import styles from './styles';
+
+const BackButton = ({onPress}) => {
+  return (
+    <TouchableOpacity style={styles.topButton} onPress={onPress}>
+      <Ionicons name="arrow-back" size={24} style={styles.icon} />
+    </TouchableOpacity>
+  );
+};
+
+export default BackButton;
