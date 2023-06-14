321da5b2b59622add601eea0ba115bbbb31082f7
John Stilia <stilia.johny@gmail.com>
2023-06-14 11:32:34 +0100

    chore(utils): Add noise color descriptions
    Former-commit-id: 6333f46b1d89a8e5ddb96d7990f81edeae620eef

--- utils/colorMap.js
+++ utils/colorMap.js
@@ -3,28 +3,34 @@ const colorMap = new Map([
   [
     'white',
     {
-      name: 'white',
+      name: 'White',
+      color: '#ffffff',
       image: require('../assets/noise_colours/white.png'),
       thumbnail: require('../assets/noise_colours/white.png'),
       audio_file: require('../assets/noise_audios/white.mp3'),
+      description: 'White noise, often compared to the gentle hiss of a radio tuned to an unused frequency, distributes energy equally across all audible frequencies, providing a soothing backdrop that can enhance focus and facilitate restful sleep.',
     },
   ],
   [
     'brown',
     {
       name: 'Brown',
+      color: '#964B00',
       image: require('../assets/noise_colours/brown.png'),
       thumbnail: require('../assets/noise_colours/brown.png'),
       audio_file: require('../assets/noise_audios/brown.mp3'),
+      description: 'Brown noise, commonly associated with the calming rumble of a waterfall or distant thunder, presents greater energy at lower frequencies, offering a deep, ambient sound that may foster relaxation and aid in promoting deeper sleep.',
     },
   ],
   [
     'pink',
     {
       name: 'Pink',
+      color: '#FFC0CB',
       image: require('../assets/noise_colours/pink.png'),
       thumbnail: require('../assets/noise_colours/pink.png'),
       audio_file: require('../assets/noise_audios/pink.mp3'),
+      description: 'Pink noise, often compared to the steady rustle of leaves in the wind, has equal energy per octave, providing a balanced mix of high and low frequency sounds that can help improve sleep and focus.',
     },
   ],
 ]);
