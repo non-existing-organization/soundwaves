7a0ed269ee147d47162e103752b28939606aff2f
John Stilia <stilia.johny@gmail.com>
2023-06-14 19:29:50 +0100

    style: remove console.log statements
    Former-commit-id: 9897a49fbf72ab464ec51b42ab773aec1341803b

--- utils/styles.js
+++ utils/styles.js
@@ -3,9 +3,6 @@ import {StyleSheet, Dimensions} from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
 
-console.log('screenWidth', screenWidth);
-
-
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
