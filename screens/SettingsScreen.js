609d110336b61bb945e5d7319a6db570fc17e671
John Stilia <stilia.johny@gmail.com>
2023-06-14 20:10:40 +0100

    feat: add fav color section with toggles
    Former-commit-id: ef3c6449991703b513222a2d4c756b2ce52ce366

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,15 +1,32 @@
 /* eslint-disable react/prop-types */
-// SettingsScreen.js
-import React from 'react';
-import {View, Text, TouchableOpacity, ImageBackground} from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, ImageBackground, Switch } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import styles from '../utils/styles';
+import colorMap from '../utils/colorMap';
 
 const backgroundImage = require('../assets/background.png');
 
-const SettingsScreen = ({navigation}) => {
+const SettingsScreen = ({ navigation, route }) => {
+  const [selectedNoises, setSelectedNoises] = useState([]);
+
   const handleBackButtonPress = () => {
     console.log('Back button pressed');
+    navigation.goBack();
+  };
+
+  const handleNoiseToggle = (colorName) => {
+    setSelectedNoises((prevSelectedNoises) => {
+      if (prevSelectedNoises.includes(colorName)) {
+        return prevSelectedNoises.filter((noise) => noise !== colorName);
+      } else {
+        return [...prevSelectedNoises, colorName];
+      }
+    });
+  };
+
+  const handleSaveSettings = () => {
+    route.params.onSelectNoises(selectedNoises);
     navigation.goBack();
   };
 
@@ -21,7 +38,27 @@ const SettingsScreen = ({navigation}) => {
         </TouchableOpacity>
       </View>
       <View style={styles.container}>
-        <Text style={{color: '#ffffff', fontSize: 20}}>This is the Settings Screen</Text>
+        <Text style={styles.segmentTitle}>Favorite Noises</Text>
+        <Text style={styles.aboutText}>Select Noises:</Text>
+        {Array.from(colorMap.values()).map((color) => (
+          <View key={color.name} style={styles.noiseItem}>
+            <View style={styles.noiseTextContainer}>
+              <Text style={styles.noiseText}>{color.name}</Text>
+            </View>
+            <View style={styles.switchContainer}>
+              <Switch
+                trackColor={{ false: '#767577', true: '#81b0ff' }}
+                thumbColor={selectedNoises.includes(color.name) ? color.color : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={() => handleNoiseToggle(color.name)}
+                value={selectedNoises.includes(color.name)}
+              />
+            </View>
+          </View>
+        ))}
+        <TouchableOpacity style={styles.saveButton} onPress={handleSaveSettings}>
+          <Text style={styles.saveButtonText}>Save</Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
