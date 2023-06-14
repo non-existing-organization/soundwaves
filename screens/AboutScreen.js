f5cbe8ff58191eb08c84032af3421dcb3a661b38
John Stilia <stilia.johny@gmail.com>
2023-06-14 11:33:19 +0100

    docs: Add color descriptions to AboutScreen
    Former-commit-id: db5325b30d6d0d739b6127c2e79341bcc6f516c8

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,19 +1,27 @@
 /* eslint-disable react/prop-types */
-
 // AboutScreen.js
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 
 import styles from '../utils/styles';
+import colorMap from '../utils/colorMap';
 
 const AboutScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.aboutText}>
-        {'Each color of noise possesses distinct frequencies and sounds that can influence our brains in different ways. White and Brown noises, with their consistent, gentle hum, can help mask disturbing ambient sounds, promoting better focus and more restful sleep.\n\n\n\n'}
-        {'White noise, often compared to the gentle hiss of a radio tuned to an unused frequency, distributes energy equally across all audible frequencies, providing a soothing backdrop that can enhance focus and facilitate restful sleep.\n\n'}
-        {'Brown noise, commonly associated with the calming rumble of a waterfall or distant thunder, presents greater energy at lower frequencies, offering a deep, ambient sound that may foster relaxation and aid in promoting deeper sleep.\n\n\n\n'}
+        {'Each color of noise possesses distinct frequencies and sounds that can influence our brains in different ways. White and Brown noises, with their consistent, gentle hum, can help mask disturbing ambient sounds, promoting better focus and more restful sleep.\n'}
+      </Text>
+      {Array.from(colorMap.values()).map((color) => (
+        <Text style={styles.aboutText} key={color.name}>
+          <Text style={{...styles.aboutText, color: color.color, fontWeight: 'bold'}}>
+            {`${color.name} `}
+          </Text>
+          {`${color.description.split(' ').slice(1).join(' ')}`}
+        </Text>
+      ))}
+      <Text style={styles.aboutText}>
         {'Through regular exposure to these therapeutic sounds, individuals may experience reduced stress, improved productivity, and better overall well-being.'}
       </Text>
       <TouchableOpacity style={styles.aboutButton} onPress={() => navigation.navigate('Main')}>
