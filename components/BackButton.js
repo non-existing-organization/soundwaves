9500e2ef3d6c347fe9f2b883e32729384d668f9d
John Stilia <stilia.johny@gmail.com>
2023-06-15 00:59:36 +0100

    feat: Add BackButton and SettingsButton components to MainScreen
    Former-commit-id: 696e568f1e7f5e514e6deb13694087e19f119288

---
+++ components/BackButton.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+// BackButton.js
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import styles from '../utils/styles';
+
+const BackButton = ({onPress}) => {
+  return (
+    <TouchableOpacity style={styles.topButton} onPress={onPress}>
+      <Ionicons name="arrow-back" size={24} style={styles.icon} />
+    </TouchableOpacity>
+  );
+};
+
+export default BackButton;
