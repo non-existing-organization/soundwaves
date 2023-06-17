885c3c1714eaf68320217a273a1be1449419c06b
John Stilia <stilia.johny@gmail.com>
2023-06-17 16:54:58 +0100

    refactor: Add new state variables for button styles
    Former-commit-id: a8b38a5ed4d9a73a30e9fe404c45e38004234e01

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,23 +1,33 @@
 /* eslint-disable react/prop-types */
 // CustomButton.js
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, TouchableOpacity, Image} from 'react-native';
 import styles from '../utils/styles';
 
-const CustomButton = ({onPress, name, image, thumbnail, isActive}) => {
+const CustomButton = ({onPress, name, image, thumbnail, isActive, isMuted}) => {
+  const [buttonStyle, setButtonStyle] = useState(styles.button);
+
+  useEffect(() => {
+    if (isActive && !isMuted) {
+      setButtonStyle([styles.button, styles.activeButton]);
+    }
+    if (!isActive && !isMuted) {
+      setButtonStyle([styles.button, styles.inactiveButton]);
+    }
+    if (isActive && isMuted) {
+      setButtonStyle([styles.button, styles.muteButton]);
+    }
+  }, [isActive, isMuted]);
+
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={isActive ? [styles.button, styles.activeButton] : styles.button}
-    >
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      {/* //FIXME store the style in a variable */}
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <Image
-          source={thumbnail}
-          style={styles.buttonImage}
-        />
+        <Image source={thumbnail} style={styles.buttonImage} />
       </View>
     </TouchableOpacity>
   );
 };
+
 
 export default CustomButton;
