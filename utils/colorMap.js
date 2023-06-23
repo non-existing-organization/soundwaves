7a4999f12e0decb5f5919a011ed22d90b0f3f234
John Stilia <stilia.johny@gmail.com>
2023-06-23 23:31:09 +0100

    feat: update main screen and remove images
    Former-commit-id: 648379909564b58e01e6e048f2aa889fa00511a8

--- utils/colorMap.js
+++ utils/colorMap.js
@@ -4,8 +4,7 @@ const colorMap = new Map([
     'white',
     {
       name: 'White',
-      color: '#ffffff',
-      image: require('../assets/noise_colours/white.png'),
+      colors: ['#f1f1f1', '#f2f2f2', '#e5e5e5', '#d9d9d9', '#cccccc'],
       thumbnail: require('../assets/thumbnails/white.png'),
       audioFile: require('../assets/noise_audios/white.mp3'),
       description: 'White noise, often compared to the gentle hiss of a radio tuned to an unused frequency, distributes energy equally across all audible frequencies, providing a soothing backdrop that can enhance focus and facilitate restful sleep.',
@@ -15,8 +14,7 @@ const colorMap = new Map([
     'brown',
     {
       name: 'Brown',
-      color: '#964B00',
-      image: require('../assets/noise_colours/brown.png'),
+      colors: ['#964B00', '#A55C00', '#B96D00', '#CC7E00', '#DF8F00'],
       thumbnail: require('../assets/thumbnails/brown.png'),
       audioFile: require('../assets/noise_audios/brown.mp3'),
       description: 'Brown noise, commonly associated with the calming rumble of a waterfall or distant thunder, presents greater energy at lower frequencies, offering a deep, ambient sound that may foster relaxation and aid in promoting deeper sleep.',
@@ -26,13 +24,13 @@ const colorMap = new Map([
     'pink',
     {
       name: 'Pink',
-      color: '#FF55FF',
-      image: require('../assets/noise_colours/pink.png'),
+      colors: ['#FF55FF', '#FF6EFF', '#FF88FF', '#FFA5FF', '#FFC1FF'],
       thumbnail: require('../assets/thumbnails/pink.png'),
       audioFile: require('../assets/noise_audios/pink.mp3'),
       description: 'Pink noise, often compared to the steady rustle of leaves in the wind, has equal energy per octave, providing a balanced mix of high and low frequency sounds that can help improve sleep and focus.',
     },
   ],
+
 
 ]);
 
