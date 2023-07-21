28d33c76880f775726822efb286253b2ca6c8bce
John Stilia <stilia.johny@gmail.com>
2023-07-21 07:44:35 +0200

    feat: #38/source audio files from remote url (#41)
    * fix: Fix loading and playing new sound
    
    * style: remove commented out code and unnecessary whitespace
    
    * Local Download and play
    If thr file doesnt exists localy it will be downloaded and then played, If the sound exists it will play it directly
    
    * update versions
    
    * refactor: Simplify handleButtonPress function and improve logging
    
    * refactor(MainScreen): remove commented code
    
    * add modal
    
    * refactor: refactor MainScreen.js and styles.js
    
    * style: add transparent color and semi-transparent color
    
    * fix: identatiobn and background colour on stop
    
    Former-commit-id: d9c55a4c32587e73d296861b99b5e3f1c2e86e59

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 // MainScreen.js
 import React, { useState, useEffect, useRef } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Modal, Text } from 'react-native';
 import { Audio } from 'expo-av';
 import { LinearGradient } from 'expo-linear-gradient';
+import * as FileSystem from 'expo-file-system';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import colorMap from '../utils/colorMap';
@@ -20,6 +22,9 @@ const MainScreen = ({ navigation }) => {
   const [isMuted, setIsMuted] = useState(false);
   const previousColorRef = useRef(null);
   const backgroundColorDefault = '#202020'; // TODO add it on the settnigs as a variable to select the default color
+  const [soundsCache, setSoundsCache] = useState(new Map());
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
 
   useEffect(() => {
@@ -35,19 +40,67 @@ const MainScreen = ({ navigation }) => {
     Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
   }, []);
 
-  // Handle button press
-  const handleButtonPress = async (colorName, image, audioFile) => {
-    console.log(`Button press detected. Color: ${colorName}, Image: ${image}, Audio File: ${audioFile}`);
 
-    const isOtherSoundPlaying = sound && audioFile !== currentAudioFile;
-    const isSoundLoaded = sound && (await sound.getStatusAsync()).isLoaded;
+  // Download audio file
+  const downloadAudioFile = async (audioFileUrl) => {
+    const uriArray = audioFileUrl.split('/');
+    const audioFile = uriArray[uriArray.length - 1];
+
+    // Define the path of the new file
+    const path = `${FileSystem.documentDirectory}${audioFile}`;
+
+    // Check if the file already exists
+    const fileInfo = await FileSystem.getInfoAsync(path);
+    if (fileInfo.exists) {
+      // console.log(`File already exists at path: ${path}`);
+      console.log(`✅ File already exists`);
+      return path;
+    } else {
+      console.log(`🛜 File does not exist, downloading new file`);
+      // If it doesn't exist, download the file
+      try {
+        // show a modal with a spinner
+        setIsLoading(true);
+        setModalVisible(true);
+        const result = await FileSystem.downloadAsync(audioFileUrl, path);
+        console.log('📊 Download result:', result.status);
+        setIsLoading(false);
+        setModalVisible(false);
+        return result.uri;
+      } catch (e) {
+        console.error('❌ Error downloading file:', e);
+        return null;
+      }
+    }
+  };
+
+
+  // Handle button press
+  const handleButtonPress = async (colorName, image, audioFileUrl, audioFileName) => {
+    console.log(`Button press detected. Color: ${colorName}, Image: ${image}, Audio File: ${audioFileUrl}`);
+
+    // Download the audio file or get the URI of the local file
+    const localAudioFileUri = await downloadAudioFile(audioFileUrl, audioFileName);
+    if (!localAudioFileUri) {
+      console.log('Error downloading sound');
+      return;
+    }
+
+    const isOtherSoundPlaying = sound && audioFileName !== currentAudioFile;
+    let isSoundLoaded = sound && (await sound.getStatusAsync()).isLoaded;
 
     console.log('isMuted:', isMuted);
 
     if (isMuted) {
-      console.log('Stoping and unloading previous sound');
-      await sound.stopAsync();
-      sound.setIsMutedAsync(false);
+      console.log('Stopping and unloading previous sound');
+      if (isSoundLoaded) {
+        try {
+          await sound.stopAsync();
+          await sound.unloadAsync();
+        } catch (e) {
+          console.error('Error stopping or unloading sound:', e);
+        }
+      }
       setIsMuted(false);
     }
 
@@ -55,54 +108,72 @@ const MainScreen = ({ navigation }) => {
 
     if (sound && isSoundLoaded && !isMuted) {
       const status = await sound.getStatusAsync();
-      if (isOtherSoundPlaying) {
-        console.log('Stopping and unloading previous sound');
-        await sound.stopAsync();
-        await sound.unloadAsync();
+      if (isOtherSoundPlaying || status.isPlaying) {
+        console.log('Stopping and unloading current sound');
+        try {
+          await sound.stopAsync();
+          await sound.unloadAsync();
+          setMainColor(backgroundColorDefault);
+
+        } catch (e) {
+          console.error('Error stopping or unloading sound:', e);
+        }
         console.log('Previous sound unloaded');
         setActiveColor(null);
-      } else if (status.isPlaying) {
-        console.log('Pausing current sound');
-        setMainColor(backgroundColorDefault);
-        await sound.pauseAsync();
-        console.log('Current sound paused');
-        setActiveColor(null);
+        if (isOtherSoundPlaying) {
+          loadAndPlayNewSound(localAudioFileUri, colorName, audioFileName);
+        }
         return;
       } else {
         console.log('Resuming current sound');
-        await sound.playAsync();
+        try {
+          await sound.playAsync();
+        } catch (e) {
+          console.error('Error playing sound:', e);
+        }
         console.log('Current sound resumed');
         setActiveColor(colorName);
         return;
       }
     }
 
-    console.log('Loading new sound');
-    const { sound: newSound } = await Audio.Sound.createAsync(
-      audioFile,
-      {
-        isLooping: true,
-        isMuted: false,
-        volume: 1.0,
-        rate: 1.0,
-        shouldCorrectPitch: true,
-      },
-      (status) => {
-        if (status.didJustFinish && !status.isLooping) {
-          setLoopCount((prevCount) => prevCount + 1);
-          console.log('Loop count:', loopCount);
-        }
-      }
-    );
-    setSound(newSound);
-    setCurrentAudioFile(audioFile);
-    setActiveColor(colorName);
-    console.log('New sound loaded');
-
-    console.log('Playing new sound');
-    await newSound.playAsync();
-    console.log('New sound playing');
+    loadAndPlayNewSound(localAudioFileUri, colorName, audioFileName);
   };
+
+  const loadAndPlayNewSound = async (audioFile, colorName, audioFileName) => {
+    console.log('Loading new sound');
+    try {
+      const { sound: newSound } = await Audio.Sound.createAsync(
+        { uri: audioFile },
+        {
+          isLooping: true,
+          isMuted: false,
+          volume: 1.0,
+          rate: 1.0,
+          shouldCorrectPitch: true,
+        },
+        (status) => {
+          if (status.didJustFinish && !status.isLooping) {
+            setLoopCount((prevCount) => prevCount + 1);
+            console.log('Loop count:', loopCount);
+          }
+        }
+      );
+      setSound(newSound);
+      setCurrentAudioFile(audioFileName);
+      setActiveColor(colorName);
+      console.log('New sound loaded');
+
+      console.log('Playing new sound');
+      await newSound.playAsync();
+      console.log('New sound playing');
+
+    } catch (e) {
+      console.error('Error loading or playing new sound:', e);
+    }
+  };
+
+
 
   // Handle speaker button press
   const handleSpeakerButtonPress = async () => {
@@ -147,33 +218,50 @@ const MainScreen = ({ navigation }) => {
   // Select a random background color
   const backgroundColor = mainColor || backgroundColorDefault ; // Default to white if no color is selected
 
-// Render the main screen
-return (
-  <View style={styles.container}>
-    <View style={styles.topBarContainer}>
-      <TouchableOpacity style={styles.speakerButton} onPress={handleSpeakerButtonPress}>
-        <Icon name={isMuted ? 'volume-off' : 'volume-up'} size={24} color={isMuted ? 'red' : 'white'} />
-      </TouchableOpacity>
-      {/* Add other elements for the top bar */}
+  // Render the main screen
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBarContainer}>
+        <TouchableOpacity style={styles.speakerButton} onPress={handleSpeakerButtonPress}>
+          <Icon name={isMuted ? 'volume-off' : 'volume-up'} size={24} color={isMuted ? 'red' : 'white'} />
+        </TouchableOpacity>
+        {/* Add other elements for the top bar */}
+      </View>
+
+      <View style={styles.modal}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {}}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.loadingText}>Downloading new sound...</Text>
+          </View>
+          <View style={styles.spinnerContainer}>
+            <Spinner visible={isLoading} />
+          </View>
+        </View>
+      </Modal>
+      </View>
+
+
+      <LinearGradient
+        colors={['black', backgroundColor, 'black']}
+        start={{ x: 0, y: 0.1 }} // Adjust the start position of the gradient
+        end={{ x: 0, y: 0.9 }} // Adjust the end position of the gradient
+        style={[styles.mainContainer, { height: 200 }]} // Adjust the height of the gradient
+      >
+        {/* Content for the main container */}
+      </LinearGradient>
+
+      <View style={styles.buttonsContainer}>
+        {/* Buttons for color selection */}
+        {Array.from(colorMap).map(renderCustomButton)}
+      </View>
     </View>
-
-
-
-    <LinearGradient
-      colors={['black', backgroundColor, 'black']}
-      start={{ x: 0, y: 0.1 }} // Adjust the start position of the gradient
-      end={{ x: 0, y: 0.9 }} // Adjust the end position of the gradient
-      style={[styles.mainContainer, { height: 200 }]} // Adjust the height of the gradient
-    >
-      {/* Content for the main container */}
-    </LinearGradient>
-
-    <View style={styles.buttonsContainer}>
-      {/* Buttons for color selection */}
-      {Array.from(colorMap).map(renderCustomButton)}
-    </View>
-  </View>
-);
+  );
 
 
 };
