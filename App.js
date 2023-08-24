1a5068801f21b96301470982b268bbb36eba801e
John Stilia <stilia.johny@gmail.com>
2023-08-25 00:13:21 +0100

    refactor: Remove commented out code
    Former-commit-id: af289208dc14f51ae7a23ea4664071d42cccb5b6

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useKeepAwake} from 'expo-keep-awake';
 
-// import AboutScreen from './src/screens/AboutScreen';
 import MainScreen from './src/screens/MainScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 
@@ -15,7 +14,6 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name="About" component={AboutScreen} /> */}
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
