8f0231e799df2e4aa42ccc533a6c0d571eb92d78
John Stilia <stilia.johny@gmail.com>
2023-08-26 15:14:19 +0100

    style: Update ESLint and Prettier configurations
    Former-commit-id: b2ea113357db1c3259e445423a58cf7e1b0fe605

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,4 @@
-// .eslintrc.js
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-      es6: true, // Add ES6 environment
-
-    },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-native/all'],
-    plugins: ['react', 'react-native'],
-    rules: {
-      'react-native/no-unused-styles': 'error',
-    },
-    "parserOptions": {
-      "sourceType": "module"
-    }
-  };
+  root: true,
+  extends: ["universe/native"],
+};
