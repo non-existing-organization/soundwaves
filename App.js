7d7d70f743e09feb6dfcfe2e88b35efd0a62e534
John Stilia <stilia.johny@gmail.com>
2023-08-26 12:16:38 +0100

    style: fix formatting in App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 // App.js
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useKeepAwake } from 'expo-keep-awake';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {useKeepAwake} from 'expo-keep-awake';
 
 import MainScreen from './src/screens/MainScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
@@ -13,7 +13,7 @@ const App = () => {
   useKeepAwake();
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
